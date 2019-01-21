f1534b5723693dfa1b10391fc644b0907bf62089
Pawel <pbalawender101@gmail.com>
2019-01-21 19:05:37 +0100

    Now it doesn't brake the board!!!

--- src/TIM2_IRQHandler.s
+++ src/TIM2_IRQHandler.s
@@ -11,11 +11,9 @@ TIM2_IRQHandler:
 	push {r0, r1, r2, lr}
 	ldr r0, =gpioa
 	ldr r0, [r0]
-	adds r0, r0, #0x18
-	ldr r1, [r0]
+	ldr r1, [r0, #0x18]
 	movs r2, #32
 	orrs r1, r2
 	str r1, [r0]
-	bl blink
 	pop {r0, r1, r2, pc}
 
--- src/main.c
+++ src/main.c
@@ -1,19 +1,11 @@
 #include<stm32f0xx.h>
 
-void configure_TIM2(void);
-
-void blink(void) {
-    GPIOA->BSRR |= (1 << 5);
-    for (int i=0; i < 100000; ++i) __NOP();
-    GPIOA->BRR |= (1 << 5);
-    for (int i=0; i < 100000; ++i) __NOP();
-}
-
 int main() {
     /* setup GPIO */
     RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
     GPIOA->MODER |= GPIO_MODER_MODER5_0;
     /* setup the timer */
+    
     RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
     TIM2->ARR = (uint32_t)0x00FFFFFF;
     TIM2->CR1 |= TIM_CR1_OPM;
@@ -22,7 +14,7 @@ int main() {
     NVIC_SetPriority(TIM2_IRQn, 0);
     TIM2->CR1 |= TIM_CR1_CEN;
     /* start timer */
-    TIM2->EGR |= TIM_EGR_UG;
+    /*TIM2->EGR |= TIM_EGR_UG;*/
 
     while (1) {}
 
