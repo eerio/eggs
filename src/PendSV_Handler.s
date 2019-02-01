2224ace7346da4a46702dac5efbfae5b10f5aad4
Pawel <pbalawender101@gmail.com>
2019-02-01 23:03:53 +0100

    Stable version of PendSV handelr

--- src/PendSV_Handler.s
+++ src/PendSV_Handler.s
@@ -1,15 +1,11 @@
 .syntax unified
-.arch armv6-m
+.cpu cortex-m0
 
 .text
 
-.extern led_on_delay, led_off_delay
 .globl PendSV_Handler
-
 .thumb_func
 PendSV_Handler:
 	cpsid i
-	push {r4, lr}
 	cpsie i
-	pop {r4, pc}
 
