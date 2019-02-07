bca1121c3969a83def9f293eeb763c37df917b7b
Pawel <pbalawender101@gmail.com>
2019-02-07 02:23:00 +0100

    Set PA5, PA6, PA7 to output mode

--- src/config.c
+++ src/config.c
@@ -15,5 +15,7 @@ void init_sys(void) {
      */
     RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
     GPIOA->MODER |= GPIO_MODER_MODER5_0;
+    GPIOA->MODER |= GPIO_MODER_MODER6_0;
+    GPIOA->MODER |= GPIO_MODER_MODER7_0;
 }
 
