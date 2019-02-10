a70098f69a7f228d004bd0d5e5ff7871b150d055
Pawel <pbalawender101@gmail.com>
2019-02-10 15:48:16 +0100

    move SPI initialization procedure to separate module

---
+++ src/spi.c
@@ -0,0 +1,89 @@
+/* SPI initialization procedure and communication abstractions
+ *
+ * author: Paweł Balawender
+ * github.com@eerio
+ */
+#include<stm32f0xx.h>
+
+/* Mode: full-duplex, master
+ * Pins:
+ *  - PA4: NSS (Slave Select), mode: output
+ *  - PA5: SCK, mode: AF0
+ *  - PA6: MISO, mode: AF0
+ *  - PA7: MOSI, mode: AF0
+ * Procedure of SPI configuration: RM p. 765
+ * SPI config:
+ *  - MSB first
+ *  - 8 bits per transfer
+ *  - clock is low when inactive (CPOL = 0)
+ *  - data is valid on clock leading edge (CPHA = 0)
+ *  - enable line is active low
+ */
+void setup_spi(void) {
+    /* Enable GPIO port A */
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+    /* SCK, MISO, MOSI: Alternate Function mode
+     * NSS: Output mode
+     */
+    GPIOA->MODER &= 0xFFFF00FF;  /* Clear GPIOA->MODER bits [15:8] */
+    GPIOA->MODER |= GPIO_MODER_MODER4_0;
+    GPIOA->MODER |= GPIO_MODER_MODER5_1;
+    GPIOA->MODER |= GPIO_MODER_MODER6_1;
+    GPIOA->MODER |= GPIO_MODER_MODER7_1;
+
+    /* Select push-pull mode */
+    GPIOA->OTYPER &= 0xFFFFFF0F;
+
+    /* Select highest frequency*/
+    GPIOA->OSPEEDR |= 0x0000FF00;
+    
+    /* Select pull-down resistors for SCK, MISO, MOSI */
+    GPIOA->PUPDR &= 0xFFFF00FF;
+    GPIOA->PUPDR |= GPIO_PUPDR_PUPDR5_1;
+    GPIOA->PUPDR |= GPIO_PUPDR_PUPDR6_1;
+    GPIOA->PUPDR |= GPIO_PUPDR_PUPDR7_1;
+
+    /* Select Alternate Function #0 for SCK, MISO, MOSI */
+    GPIOA->AFR[0] &= 0x000FFFFF;
+
+    /* Enable SPI #1 clock */
+    RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
+
+    /* Data mode: 2-line unidirectional */
+    SPI1->CR1 &= ~SPI_CR1_BIDIMODE;
+    /* Disable CRC */
+    SPI1->CR1 &= ~SPI_CR1_CRCEN;
+    /* Full-duplex mode */
+    SPI1->CR1 &= ~SPI_CR1_RXONLY;
+    /* Software slave management */
+    SPI1->CR1 |= SPI_CR1_SSM;
+    /* Internal slave select */
+    SPI1->CR1 |= SPI_CR1_SSI;
+    /* MSB first */
+    SPI1->CR1 &= ~SPI_CR1_LSBFIRST;
+    /* Set baudrate to PCLK / 32 */
+    SPI1->CR1 &= SPI_CR1_BR;
+    SPI1->CR1 |= (0b100) << SPI_CR1_BR_Pos;
+    /* Master configuration */
+    SPI1->CR1 |= SPI_CR1_MSTR;
+    /* Clock to 0 when idle */
+    SPI1->CR1 &= ~SPI_CR1_CPOL;
+    /* First clock transition is the first data capture edge */
+    SPI1->CR1 &= ~SPI_CR1_CPHA;
+
+    /* RXNE if the FIFO level >= 8 bit */
+    SPI1->CR2 |= SPI_CR2_FRXTH;
+    /* Transfer data length: 8-bit */
+    SPI1->CR2 &= ~SPI_CR2_DS;
+    SPI1->CR2 |= 0b0111 << SPI_CR2_DS_Pos;
+    /* Frame format: SPI Motorola mode */
+    SPI1->CR2 &= ~SPI_CR2_FRF;
+    /* Send NSS pulse between two data transfers */
+    SPI1->CR2 |= SPI_CR2_NSSP;
+    /* Slave Select Output disable */
+    SPI1->CR2 &= ~SPI_CR2_SSOE;
+
+    /* Enable SPI */
+    SPI1->CR1 |= SPI_CR1_SPE;
+}
+
