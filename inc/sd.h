4a525d1c62cee617fadfa54b1a8dfde15dcb2abd
Pawel <pbalawender101@gmail.com>
2019-02-19 20:13:26 +0100

    Add SD initialization procedures

---
+++ inc/sd.h
@@ -0,0 +1,6 @@
+/* Prototypes for SD abstractions and initialization procedures
+ *
+ * author: Paweł Balawender
+ * github.com@eerio
+ */
+
---
+++ src/sd.c
@@ -0,0 +1,8 @@
+/* SDHC card initialization procedure and communication abstractions
+ *
+ * author: Paweł Balawender
+ * github.com@eerio
+ */
+#include<sd.h>
+#include<spi.h>
+
