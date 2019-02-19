ad7df90c85f1fe526ddeef14433300803c5299a7
Pawel <pbalawender101@gmail.com>
2019-02-19 20:11:31 +0100

    Remove SPI_send procedure

--- inc/spi.h
+++ inc/spi.h
@@ -5,9 +5,6 @@
  */
 #include<stm32f0xx.h>
 
-/* Send a sequence of bytes by the SPI interface */
-void SPI_send(unsigned int, uint8_t []);
-
 /* Initialize SPI peripheral */
 void setup_spi(void);
 
