e6e898c2469fda323a41cda21c88283901c95b33
Pawel <pbalawender101@gmail.com>
2019-02-23 18:04:23 +0100

    NSS software management successful

--- src/sd.c
+++ src/sd.c
@@ -5,8 +5,14 @@
  */
 #include<sd.h>
 #include<spi.h>
+#include<common.h>
 #define SD_SPI (SPI1)
 #define SD_GPIO (GPIOA) 
+
+
+uint8_t blank[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+uint8_t cmd0[] = {0b01000000, 0, 0, 0, 0, 0b10010101};
+
 
 void init_sd(void) {
     /* Save SPI settings */
@@ -14,22 +20,19 @@ void init_sd(void) {
              cr1 = SD_SPI->CR1,
              cr2 = SD_SPI->CR2;
 
-    /* Change NSS mode to output */
-    SD_GPIO->MODER &= ~GPIO_MODER_MODER4;
-    SD_GPIO->MODER |= GPIO_MODER_MODER4_0;
     /* Change NSS management to software */
     SD_SPI->CR1 |= SPI_CR1_SSM;
-    SPI1->CR2 &= ~SPI_CR2_NSSP;
-    SPI1->CR2 &= ~SPI_CR2_SSOE;
+    /* Select output mode for NSS pin */
+    SD_GPIO->MODER &= ~GPIO_MODER_MODER4;
+    SD_GPIO->MODER |= GPIO_MODER_MODER4_0;
 
-    /* Set NSS high */
+    /* Set NSS high and set MOSI high for 80 cycles (at least 74) */
     SD_GPIO->ODR |= (1 << 4);
-    /* Set MOSI high for 80 cycles (at least 74, but aligned to 8) */
-    uint8_t blank[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-    spi_send(blank);
+    for (unsigned int i=0; i < 10; ++i) spi_send(blank);
 
-    /* Send CMD0 */
-    uint8_t cmd0[] = {0b01000000, 0, 0, 0, 0, 0b10010101};
+    /* Set NSS low and send CMD0 */
+    SD_GPIO->ODR &= ~(1 << 4);
+    spi_send(cmd0);
     spi_send(cmd0);
 
     /* Revert settings */
