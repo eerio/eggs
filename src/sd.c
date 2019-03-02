e9fd1588d17d59569dc69e2bda22efface4f1673
Pawel <pbalawender101@gmail.com>
2019-03-02 19:12:05 +0100

    Finish SD initializing sequence

--- src/sd.c
+++ src/sd.c
@@ -12,8 +12,10 @@
 
 uint8_t blank[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 uint8_t cmd0[] = {0x40, 0x00, 0x00, 0x00, 0x00, 0x95};
-uint8_t cmd8[] = {0b01001000, 0x00, 0x00, 0x01, 0b10101010, 0b00001111};
-uint8_t cmd58[] = {0b01111010, 0, 0, 0, 0, 0b01110101};
+uint8_t cmd8[] = {0b01001000, 0x00, 0x00, 0x01, 0b10101010,1}; //0b00001111};
+uint8_t cmd58[] = {0b01111010, 0, 0, 0, 0, 1};//0b01110101};
+uint8_t cmd55[] = {0b01110111, 0, 0, 0, 0, 1};
+uint8_t acmd41[] = {0b01101001, 0, 0, 0, 0, 1};
 
 
 void init_sd(void) {
@@ -21,6 +23,7 @@ void init_sd(void) {
     uint32_t moder = SD_GPIO->MODER,
              cr1 = SD_SPI->CR1,
              cr2 = SD_SPI->CR2;
+    volatile uint8_t *resp;
 
     /* Change NSS management to software */
     SD_SPI->CR1 |= SPI_CR1_SSM;
@@ -33,21 +36,53 @@ void init_sd(void) {
     spi_send(blank);
     spi_send(blank);
 
-    /* Assert NSS and send CMD0 */
+    /* Assert NSS to send commands */
     SD_GPIO->ODR &= ~(1 << 4);
-    spi_send(cmd0);
-    /* TODO: Is it really needed? */
-    spi_send(blank);
-    spi_send(blank);
-    spi_send(blank);
-    /* CMD8 not supported */
+
+    /* Spam CMD0 */
+    do {
+        spi_send(cmd0);
+        spi_send(blank);
+        resp = spi_read();
+    } while (*resp == 0xFF);
+
+    /* Read SD card's OCR register and give clock for at least 6+1 bytes */
     spi_send(cmd58);
-    /* Keep the clock for a while */
-    for (unsigned int i=0; i < 4; ++i) spi_send(blank);
-    
-    SD_GPIO->ODR |= (1 << 4);
+    spi_send(blank);
+    spi_send(blank)
+    resp = spi_read();
+
+    /* Assert no errors in R1 */
+    if (resp[0] != 0x01) while (1) {
+        /* Not an SD card (invalid response for CMD58 command) */
+    }
+    /* Assert voltage OK  (bit 20: 3.2-3.3V, bit 32: 3.3-3.4V) */
+    /* TODO: it should only check 3.3V bits! */
+    if ((resp[2] != 0xFF) || (resp[3] != 0x80)) while (1) {
+        /* Invalid operating voltage range */
+    }
+
+    /* Spam ACMD41 until SD card's not busy (so it's ready)
+     * preceed ACMD41 with CMD55 to inform SD that the next
+     * command is special; ACMD41 starts SD initialization process
+     *
+     * ref: p.227 PhysicalLayerSimplifiedSpecification, p.7 lec12
+     */
+    spi_send(cmd55);
+    spi_send(acmd41);
+    spi_send(blank);
+
+    /* Loop until card's power up procedure is finished
+     * ref: SD Product Manual, p. 37
+     */
+    do {
+        spi_send(cmd58);
+        spi_send(blank);
+        resp = spi_read();
+    } while ((resp[1] & (1 << 7))== 0);
 
     /* Revert settings */
+    SD_GPIO->ODR |= (1 << 4);
     SD_SPI->CR1 = cr1;
     SD_SPI->CR2 = cr2;
     SD_GPIO->MODER = moder;
