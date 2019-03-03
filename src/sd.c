edccd260aa2c8c020b94287e99c71c836b1890da
Pawel <pbalawender101@gmail.com>
2019-03-03 14:40:14 +0100

    Remove redundant code, update commands

--- src/sd.c
+++ src/sd.c
@@ -12,39 +12,40 @@
 
 uint8_t blank[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 uint8_t cmd0[] = {0x40, 0x00, 0x00, 0x00, 0x00, 0x95};
-uint8_t cmd8[] = {0b01001000, 0x00, 0x00, 0x01, 0b10101010,1}; //0b00001111};
-uint8_t cmd58[] = {0b01111010, 0, 0, 0, 0, 1};//0b01110101};
+uint8_t cmd8[] = {0b01001000, 0x00, 0x00, 0x01, 0xAA, 0b00001111};
+uint8_t cmd58[] = {0b01111010, 0, 0, 0, 0, 0b01110101};
 uint8_t cmd55[] = {0b01110111, 0, 0, 0, 0, 1};
 uint8_t acmd41[] = {0b01101001, 0, 0, 0, 0, 1};
 
 
 void init_sd(void) {
-    /* Save SPI settings */
-    uint32_t moder = SD_GPIO->MODER,
-             cr1 = SD_SPI->CR1,
-             cr2 = SD_SPI->CR2;
     volatile uint8_t *resp;
 
-    /* Change NSS management to software */
-    //SD_SPI->CR1 |= SPI_CR1_SSM;
-    /* Select output mode for NSS pin */
-    //SD_GPIO->MODER &= ~GPIO_MODER_MODER4;
-    //SD_GPIO->MODER |= GPIO_MODER_MODER4_0;
-
-    /* Set NSS high and set MOSI high for 96 cycles (at least 74) */
-    //SD_GPIO->ODR |= (1 << 4);
+    /* Toggle CLK for 96 cycles (at least 74) */
     spi_send(blank);
     spi_send(blank);
 
-    /* Assert NSS to send commands */
-    //SD_GPIO->ODR &= ~(1 << 4);
-
-    /* Spam CMD0 */
+    /* Spam CMD0 until there's a response */
     do {
         spi_send(cmd0);
         spi_send(blank);
         resp = spi_read();
     } while (*resp == 0xFF);
+
+    /* Send CMD8, check if Illegal Command in responce */
+    spi_send(cmd8);
+    spi_send(blank);
+    resp = spi_read();
+
+    if (*resp & (1 << 2)) {
+        /* Illegal command -> Ver 1.X SD card or not a SD card */
+    } else {
+        /* No Illegal command in reponce -> Ver >= 2.0 SD card
+         * Check if CRC error or non-compatible voltage range
+         */
+
+
+    }
 
     /* Read SD card's OCR register and give clock for at least 6+1 bytes */
     spi_send(cmd58);
@@ -81,11 +82,5 @@ void init_sd(void) {
         spi_send(blank);
         resp = spi_read() - 6;
     } while ((resp[1] & (1 << 7))== 0);
-
-    /* Revert settings */
-    //SD_GPIO->ODR |= (1 << 4);
-    SD_SPI->CR1 = cr1;
-    SD_SPI->CR2 = cr2;
-    SD_GPIO->MODER = moder;
 }
 
