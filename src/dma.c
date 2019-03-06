e94fc48f22d10dbe65f8f2ee9a280611d27782ba
Pawel <pbalawender101@gmail.com>
2019-03-06 22:08:32 +0100

    Add Buffer overflow handling

--- src/dma.c
+++ src/dma.c
@@ -61,6 +61,7 @@ void DMA1_Ch2_3_DMA2_Ch1_2_IRQHandler(void) {
         SPI1->CR2 &= ~SPI_CR2_TXDMAEN;
         DMA1->IFCR |= DMA_IFCR_CTCIF3;
     } else if (DMA1->ISR & DMA_ISR_TCIF2) {
+        while(1) { /* Overflow */ }
         SPI1->CR2 &= ~SPI_CR2_RXDMAEN;
         DMA1->IFCR |= DMA_IFCR_CTCIF2;
     }
