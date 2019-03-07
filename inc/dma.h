6246620e5aa4cb763a338c0f4d73838fe8700233
Pawel <pbalawender101@gmail.com>
2019-03-07 13:06:10 +0100

    Add changeable dma periph support

--- inc/dma.h
+++ inc/dma.h
@@ -1,4 +1,37 @@
+#include<stm32f0xx.h>
+
+#define DMA_SPI_RX DMA1_Channel2
+#define DMA_SPI_TX DMA1_Channel3
+#define DMA_SPI_RX_DMA (DMA1)
+#define DMA_SPI_TX_DMA (DMA1)
+#define DMA_SPI_RX_Channel (2)
+#define DMA_SPI_TX_Channel (3)
+
+
 void configure_DMA(void);
 void start_DMA(void);
-void DMA1_Ch2_3_DMA2_Ch1_2_IRQHandler(void);
 void disable_dma(void);
+
+/* According to p. 198 RefMan, DMA can be mapped to:
+ * SPI1_RX: DMA1_Channel2 or DMA2_Channel3
+ * SPI1_TX: DMA1_Channel3 or DMA2_Channel4
+ * SPI2_RX: DMA1_Channel4 or DMA1_Channel6
+ * SPI2_TX: DMA1_Channel5 or DMA1_Channel7
+ */
+#if DMA_SPI_RX == DMA1_Channel2 \
+ || DMA_SPI_TX == DMA1_Channel3
+
+void DMA1_Ch2_3_DMA2_Ch1_2_IRQHandler(void);
+#endif
+
+
+#if DMA_SPI_RX == DMA2_Channel3 \
+ || DMA_SPI_RX == DMA1_Channel4 \
+ || DMA_SPI_RX == DMA2_Channel6 \
+ || DMA_SPI_TX == DMA2_Channel4 \
+ || DMA_SPI_TX == DMA1_Channel5 \
+ || DMA_SPI_TX == DMA1_Channel7
+
+void DMA1_Ch4_7_DMA2_Ch3_5_IRQHandler(void);
+#endif
+
--- src/dma.c
+++ src/dma.c
@@ -2,77 +2,61 @@
 #include<common.h>
 #include<dma.h>
 
-#define DMA_RX (DMA1_Channel2)
-#define DMA_RX_DMA (DMA1)
-#define DMA_RX_Channel (2)
-#define DMA_TX (DMA1_Channel3)
-#define DMA_TX_DMA (DMA1)
-#define DMA_TX_Channel (3)
-
-
 void configure_DMA(void) {
     /* Perform each time 6 transfers from SPI TX buffer to SPI1->DR */
-    if ((DMA_TX->CCR & DMA_CCR_EN) == 0) {
-        DMA_TX->CPAR = (uint32_t)(&(SPI1->DR));
-        DMA_TX->CMAR = (uint32_t)(&SPI_TX_buffer);
-        DMA_TX->CNDTR |= (TX_BUFFER_SIZE << DMA_CNDTR_NDT_Pos);
+    if ((DMA_SPI_TX->CCR & DMA_CCR_EN) == 0) {
+        DMA_SPI_TX->CPAR = (uint32_t)(&(SPI1->DR));
+        DMA_SPI_TX->CMAR = (uint32_t)(&SPI_TX_buffer);
+        DMA_SPI_TX->CNDTR |= (TX_BUFFER_SIZE << DMA_CNDTR_NDT_Pos);
     }
     /* Perform each time 1 transfer from SPI1 RX FIFO to the buffer */
-    if ((DMA_RX->CCR & DMA_CCR_EN) == 0) {
-        DMA_RX->CPAR = (uint32_t)(&(SPI1->DR));
-        DMA_RX->CMAR = (uint32_t)(&SPI_RX_buffer);
-        DMA_RX->CNDTR |= (RX_BUFFER_SIZE << DMA_CNDTR_NDT_Pos);
+    if ((DMA_SPI_RX->CCR & DMA_CCR_EN) == 0) {
+        DMA_SPI_RX->CPAR = (uint32_t)(&(SPI1->DR));
+        DMA_SPI_RX->CMAR = (uint32_t)(&SPI_RX_buffer);
+        DMA_SPI_RX->CNDTR |= (RX_BUFFER_SIZE << DMA_CNDTR_NDT_Pos);
     }
 
     /* Memory to peripheral mode */
-    DMA_TX->CCR &= ~DMA_CCR_MEM2MEM;
-    DMA_RX->CCR &= ~DMA_CCR_MEM2MEM;
+    DMA_SPI_TX->CCR &= ~DMA_CCR_MEM2MEM;
+    DMA_SPI_RX->CCR &= ~DMA_CCR_MEM2MEM;
     /* Channel priority: Very high */
-    DMA_TX->CCR |= DMA_CCR_PL;
-    DMA_RX->CCR |= DMA_CCR_PL;
+    DMA_SPI_TX->CCR |= DMA_CCR_PL;
+    DMA_SPI_RX->CCR |= DMA_CCR_PL;
     /* Data size in TX FIFO: 8 bits */
-    DMA_TX->CCR &= ~DMA_CCR_MSIZE;
-    DMA_RX->CCR &= ~DMA_CCR_MSIZE;
+    DMA_SPI_TX->CCR &= ~DMA_CCR_MSIZE;
+    DMA_SPI_RX->CCR &= ~DMA_CCR_MSIZE;
     /* Data size in SPI DR: 8 bits */
-    DMA_TX->CCR &= ~DMA_CCR_PSIZE;
-    DMA_RX->CCR &= ~DMA_CCR_PSIZE;
+    DMA_SPI_TX->CCR &= ~DMA_CCR_PSIZE;
+    DMA_SPI_RX->CCR &= ~DMA_CCR_PSIZE;
     /* Increment memory address */
-    DMA_TX->CCR |= DMA_CCR_MINC;
-    DMA_RX->CCR |= DMA_CCR_MINC;
+    DMA_SPI_TX->CCR |= DMA_CCR_MINC;
+    DMA_SPI_RX->CCR |= DMA_CCR_MINC;
     /* Don't increment peripheral address */
-    DMA_TX->CCR &= ~DMA_CCR_PINC;
-    DMA_RX->CCR &= ~DMA_CCR_PINC;
+    DMA_SPI_TX->CCR &= ~DMA_CCR_PINC;
+    DMA_SPI_RX->CCR &= ~DMA_CCR_PINC;
     /* Circular mode */
-    DMA_TX->CCR |= DMA_CCR_CIRC;
-    DMA_RX->CCR |= DMA_CCR_CIRC;
+    DMA_SPI_TX->CCR |= DMA_CCR_CIRC;
+    DMA_SPI_RX->CCR |= DMA_CCR_CIRC;
     /* Transfer direction: Memory to peripheral */
-    DMA_TX->CCR |= DMA_CCR_DIR;
-    DMA_RX->CCR &= ~DMA_CCR_DIR; /* Periph to mem */
+    DMA_SPI_TX->CCR |= DMA_CCR_DIR;
+    DMA_SPI_RX->CCR &= ~DMA_CCR_DIR; /* Periph to mem */
 
     /* Transfer complete interrupt enable */
-    DMA_TX->CCR |= DMA_CCR_TCIE;
-    //DMA_RX->CCR |= DMA_CCR_TCIE;
+    DMA_SPI_TX->CCR |= DMA_CCR_TCIE;
+    //DMA_SPI_RX->CCR |= DMA_CCR_TCIE;
     NVIC_EnableIRQ(DMA1_Ch2_3_DMA2_Ch1_2_IRQn);
     NVIC_SetPriority(DMA1_Ch2_3_DMA2_Ch1_2_IRQn, 0);
 }
-
-void DMA1_Ch2_3_DMA2_Ch1_2_IRQHandler(void) {
-    if (DMA1->ISR & DMA_ISR_TCIF3) {
-        SPI1->CR2 &= ~SPI_CR2_TXDMAEN;
-        DMA1->IFCR |= DMA_IFCR_CTCIF3;
-    }
-}
-
 void start_DMA(void) {
     /* Enable channels */
-    DMA_TX->CCR |= DMA_CCR_EN;
-    DMA_RX->CCR |= DMA_CCR_EN;
+    DMA_SPI_TX->CCR |= DMA_CCR_EN;
+    DMA_SPI_RX->CCR |= DMA_CCR_EN;
 }
 
 void disable_dma(void) {
     /* Disable channels */
-    while(DMA_TX->CNDTR != 6) {}
-    DMA_TX->CCR &= ~DMA_CCR_EN;
-    DMA_RX->CCR &= ~DMA_CCR_EN;
+    while(DMA_SPI_TX->CNDTR != 6) {}
+    DMA_SPI_TX->CCR &= ~DMA_CCR_EN;
+    DMA_SPI_RX->CCR &= ~DMA_CCR_EN;
 }
 
