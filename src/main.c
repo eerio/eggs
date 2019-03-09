f52482f07e284b0de10e0654e274818056bab3ab
Pawel <pbalawender101@gmail.com>
2019-03-09 15:23:22 +0100

    Move PFF example to separate procedure

--- src/main.c
+++ src/main.c
@@ -12,10 +12,10 @@
  */
 #include<config.h>
 #include<pff.h>
+#include<stdio.h>
 
-void die(FRESULT rc) {
-    while(1) { /* Failed with passed rc */ }
-}
+void die(FRESULT rc);
+void test_pff(void);
 
 int main(void) {
     /* At this point we assume that the stack is initialized,
@@ -26,27 +26,81 @@ int main(void) {
      * startup_<device>.s file and SystemInit func in system_<device_fam>.c
      */
     init_sys();
-
-    FATFS fatfs;
-    FRESULT rc = pf_mount(&fatfs);
-    unsigned int br;
-    uint8_t buff[64];
-
-    if (rc) die(rc);
-
-    rc = pf_open("hi.txt");
-    if (rc) die(rc);
-
-    rc = 0;
-    br = 1;
-    while(!rc && br) {
-        rc = pf_read(buff, sizeof(buff), &br);
-    }
-    if (rc) die(rc);
-    
+    test_pff();
     quit_sys();
     /* Main thread after return from the main function goes to an infinite
      * loop in the startup_stm32f091xc.s file */
     return 0;
+}
+
+void die (		/* Stop with dying message */
+	FRESULT rc	/* FatFs return value */
+)
+{
+	printf("Failed with rc=%u.\n", rc);
+	for (;;) ;
+}
+
+void test_pff(void) {
+	FATFS fatfs;			/* File system object */
+	DIR dir;				/* Directory object */
+	FILINFO fno;			/* File information object */
+	UINT br, i;
+	BYTE buff[64];
+
+
+	printf("\nMount a volume.\n");
+	FRESULT rc = pf_mount(&fatfs);
+	if (rc) die(rc);
+
+	printf("\nOpen a test file (message.txt).\n");
+	rc = pf_open("MESSAGE.TXT");
+	if (rc) die(rc);
+
+	printf("\nType the file content.\n");
+	for (;;) {
+		rc = pf_read(buff, sizeof(buff), &br);	/* Read a chunk of file */
+		if (rc || !br) break;			/* Error or end of file */
+		for (i = 0; i < br; i++)		/* Type the data */
+			putchar(buff[i]);
+	}
+	if (rc) die(rc);
+
+#if PF_USE_WRITE
+	printf("\nOpen a file to write (write.txt).\n");
+	rc = pf_open("WRITE.TXT");
+	if (rc) die(rc);
+
+	printf("\nWrite a text data. (Hello world!)\n");
+	for (;;) {
+		rc = pf_write("Hello world!\r\n", 14, &bw);
+		if (rc || !bw) break;
+	}
+	if (rc) die(rc);
+
+	printf("\nTerminate the file write process.\n");
+	rc = pf_write(0, 0, &bw);
+	if (rc) die(rc);
+#endif
+
+#if PF_USE_DIR
+	printf("\nOpen root directory.\n");
+	rc = pf_opendir(&dir, "");
+	if (rc) die(rc);
+
+	printf("\nDirectory listing...\n");
+	for (;;) {
+		rc = pf_readdir(&dir, &fno);	/* Read a directory item */
+		if (rc || !fno.fname[0]) break;	/* Error or end of dir */
+		if (fno.fattrib & AM_DIR)
+			printf("   <dir>  %s\n", fno.fname);
+		else
+			printf("%8lu  %s\n", fno.fsize, fno.fname);
+	}
+	if (rc) die(rc);
+#endif
+
+	printf("\nTest completed.\n");
+	for (;;) ;    
 }
 
