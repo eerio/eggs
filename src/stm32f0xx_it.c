d49205530f9571b84179f34f2cb533e2d47e2b0a
Pawel <pbalawender101@gmail.com>
2019-03-10 21:37:06 +0100

    Hang at buffer overflow

--- src/stm32f0xx_it.c
+++ src/stm32f0xx_it.c
@@ -9,7 +9,7 @@ void DMA1_Ch2_3_DMA2_Ch1_2_IRQHandler(void) {
         SPI1->CR2 &= ~SPI_CR2_TXDMAEN;
         DMA1->IFCR |= DMA_IFCR_CTCIF3;
     } else if (DMA_SPI_RX_DMA->ISR & DMA_ISR_TCIF2) {
-        SPI_RX_ind = 0;
+        while(1) { /* Buffer overflow */ }
         DMA1->IFCR |= DMA_IFCR_CTCIF2;
     }
 }
