9c9a297ffb92ed7c3c2c40c219cfb418afd9ce90
Pawel <pbalawender101@gmail.com>
2019-03-10 20:08:32 +0100

    Add MBR, PartitionEntry structures definitions

--- inc/sd.h
+++ inc/sd.h
@@ -8,3 +8,19 @@
 DSTATUS sd_initialize (void);
 DRESULT sd_readp(BYTE*, DWORD, UINT, UINT);
 
+typedef struct {
+    uint8_t status;
+    uint8_t chs_addr_start[3];
+    uint8_t type;
+    uint8_t chs_addr_end[3];
+    uint32_t lba_addr;
+    uint32_t sectors_num;
+} PartitionEntry;
+
+typedef struct {
+    uint8_t bootstrap_code[446];
+    PartitionEntry PartitionTable[4];
+    uint32_t boot_signature;
+} MBR;
+
+
