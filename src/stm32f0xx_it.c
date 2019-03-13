8e7da607144ff0f6707a800b8b66104868d813e2
Pawel <pbalawender101@gmail.com>
2019-03-13 23:15:08 +0100

    Remove disabling DMA on TC IRQ

--- src/stm32f0xx_it.c
+++ src/stm32f0xx_it.c
@@ -5,10 +5,8 @@
 #if defined(USE_DEFAULT_SPI_SD_TX_DMA) || defined(USE_DEFAULT_SPI_SD_RX_DMA)
 void DMA1_Ch2_3_DMA2_Ch1_2_IRQHandler(void) {
     if (DMA_SPI_TX_DMA->ISR & DMA_ISR_TCIF3) {
-        SPI1->CR2 &= ~SPI_CR2_TXDMAEN;
         DMA1->IFCR |= DMA_IFCR_CTCIF3;
     } else if (DMA_SPI_RX_DMA->ISR & DMA_ISR_TCIF2) {
-        //while(1) { /* Buffer overflow */ }
         DMA1->IFCR |= DMA_IFCR_CTCIF2;
     }
 }
