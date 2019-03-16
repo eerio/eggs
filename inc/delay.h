fe634ab250f1f574ba5e642a86a9a238d1026561
Pawel <pbalawender101@gmail.com>
2019-03-16 10:57:17 +0100

    Add docstring

--- inc/delay.h
+++ inc/delay.h
@@ -1,2 +1,32 @@
-void delay(unsigned int);
+/**
+ * @file delay.h
+ * @brief Delay function prototype
+ * @author Pawel Balawender
+ * @version Alpha
+ * @date 2019-03-15
+ *
+ * Copyright (C) 
+ * 2019 - Pawel Balawender
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ */
+
+/**
+ * @brief Execute redundant code for 12 + 4n cycles
+ *
+ * @param int n Number of empty loops to execute
+ */
+void delay(unsigned int n);
 
