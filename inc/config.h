c7751bfa550298cb2e5dc9b5a9243b07102c79d7
Pawel <pbalawender101@gmail.com>
2019-03-16 10:56:59 +0100

    Add docstring, move here SD TX/RX config

--- inc/config.h
+++ inc/config.h
@@ -1,8 +1,34 @@
-/* Configuration procedures prototypes
+/**
+ * @file config.h2019 - Pawel Balawender
+ * @brief Config header
+ * To change configuration comment/uncomment particular macros
  *
- * author: Paweł Balawender
- * github.com@eerio
+ * @author Pawel Balawender
+ * @version Alpha
+ * @date 2019-03-15
+ *
+ * Copyright (C) 
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
  */
 
+
+/* Ifndef: SD card driver will use SPI2 peripheral */
 #define SD_USE_SPI1
+
+#define SD_USE_DEFAULT_DMA_TX
+#define SD_USE_DEFAULT_DMA_RX
 
